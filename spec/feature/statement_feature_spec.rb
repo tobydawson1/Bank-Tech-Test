73f2954dacb410423cb4e62fe1ac6e6ddc563ae5
Toby Dawson <tobydawson1@gmail.com>
2020-03-26 09:46:48 +0000

    fixed date for tests

--- spec/feature/statement_feature_spec.rb
+++ spec/feature/statement_feature_spec.rb
@@ -24,20 +24,20 @@ describe 'features' do
   it 'will print statements' do
     @account.deposit(10)
     @account.deposit(50)
-    expect(@account.print_statement).to contain_exactly("date  || credit  || debit  || balance", "date 25/03/2020 || credit 10.00 || debit 0.00 || balance 10.00", "date 25/03/2020 || credit 50.00 || debit 0.00 || balance 60.00")
+    expect(@account.print_statement).to contain_exactly("date  || credit  || debit  || balance", "date #{@date} || credit 10.00 || debit 0.00 || balance 10.00", "date #{@date} || credit 50.00 || debit 0.00 || balance 60.00")
   end
 
   it 'will withdraw' do
     @account.deposit(40)
     @account.withdraw(10)
-    expect(@account.print_statement).to contain_exactly("date  || credit  || debit  || balance", "date 25/03/2020 || credit 40.00 || debit 0.00 || balance 40.00", "date 25/03/2020 || credit 0.00 || debit 10.00 || balance 30.00")
+    expect(@account.print_statement).to contain_exactly("date  || credit  || debit  || balance", "date #{@date} || credit 40.00 || debit 0.00 || balance 40.00", "date #{@date} || credit 0.00 || debit 10.00 || balance 30.00")
   end
 
   it 'will deposit after a withdraw' do
     @account.deposit(40)
     @account.withdraw(10)
     @account.deposit(20)
-    expect(@account.print_statement).to contain_exactly("date  || credit  || debit  || balance", "date 25/03/2020 || credit 40.00 || debit 0.00 || balance 40.00", "date 25/03/2020 || credit 0.00 || debit 10.00 || balance 30.00", "date 25/03/2020 || credit 20.00 || debit 0.00 || balance 50.00")
+    expect(@account.print_statement).to contain_exactly("date  || credit  || debit  || balance", "date #{@date} || credit 40.00 || debit 0.00 || balance 40.00", "date #{@date} || credit 0.00 || debit 10.00 || balance 30.00", "date #{@date} || credit 20.00 || debit 0.00 || balance 50.00")
   end 
 end
   
